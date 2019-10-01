8b40fbaddef929a50c2b0b62724038344b1c5be0
Matheus Vinicius da Costa <ma.usv@hotmail.com>
2019-10-01 00:26:17 -0300

    Update main.c
    Atribuir o nome informado para o nome do produto

--- main.c
+++ main.c
@@ -4,10 +4,18 @@
 int main(int argc, char** argv)
 {
 	char str[50];
+	controle ctrl;
 	
 	
-	printf("Implementar controle de estoque: ");
+	printf("Começar o controle? <Sim/Nao> ");
 	gets(str);
+	if(strcmp(strupr(str),"SIM"));
+	{
+		printf("Informe um nome para o produto: ");
+		fflush(stdin);
+		gets(str);
+		strcpy(ctrl.nome,str);
+	}
 	printf("%s",str);
 	return 0;
 }
